b06cdf58748de560675fe548ef07a611d3ca0926
NouhaSejil <160036551+NouhaSejil@users.noreply.github.com>
2024-02-20 23:12:55 +0100

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,26 @@
+package com.idl.repository;
+
+
+
+	import org.springframework.data.jpa.repository.JpaRepository;
+	import org.springframework.stereotype.Repository;
+
+	import com.idl.entities.User;
+
+
+
+	@Repository
+	public interface UserRepository extends JpaRepository<User,Long> {
+
+
+		User findByFname(String fname);
+
+
+	
+
+		
+
+			
+
+	}
+
