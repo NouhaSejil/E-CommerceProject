42358a6a30dfbc66a89ff0bacf8a4d01acf36d7f
NouhaSejil <160036551+NouhaSejil@users.noreply.github.com>
2024-02-21 12:55:27 +0100

    Add files via upload

--- demo/src/main/java/com/example/demo/repository/UserRepository.java
+++ demo/src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,11 +1,11 @@
-package com.idl.repository;
+package com.example.demo.repository;
 
 
 
 	import org.springframework.data.jpa.repository.JpaRepository;
 	import org.springframework.stereotype.Repository;
 
-	import com.idl.entities.User;
+	import com.example.demo.entity.User;
 
 
 
